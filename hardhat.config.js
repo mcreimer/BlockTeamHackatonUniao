37691afef9dc3cc8dd280a768655f2a70cf03820
Marcelo Creimer <14227072+mcreimer@users.noreply.github.com>
2022-12-14 08:30:35 -0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,11 +7,7 @@ require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true
-    }
-  },
+
   solidity: {
     compilers: [
       {
@@ -34,16 +30,17 @@ module.exports = {
       }
     ]
   },
-  networks: {
+  networks: {    
     alfajores: {
       url: "https://alfajores-forno.celo-testnet.org",
       accounts: {
         mnemonic: process.env.MNEMONIC,
         path: "m/44'/52752'/0'/0"
       },
-      chainId: 44787
+      chainId: 44787 ,     
+      gas: 2100000,
+      gasPrice: 8000000000
     }
   },
 };
-
 
